13814239d25abcb6feeb7dc65c0b1a752d34dffb
unattendcode <47460712+unattendcode@users.noreply.github.com>
2021-10-14 09:14:55 +0000

    full firewall create script

--- createfirewall.ps1
+++ createfirewall.ps1
@@ -4,11 +4,13 @@ $FWNAME="Team3AzureFirewall"
 $LOC="northeurope"
 $FWIPCONFIG_NAME="FirewallIPConfig"
 $VNET_NAME="vnet"
+$AKSSUBNET_NAME="aks-subnet"
 $SUBID="5239decf-2a5b-497d-9f85-b64f45c13a65"
 $PREFIX="Team3"
 $FWROUTE_TABLE_NAME="${PREFIX}-fwrt"
 $FWROUTE_NAME="${PREFIX}-fwrn"
 $FWROUTE_NAME_INTERNET="${PREFIX}-fwinternet"
+$SERVICE_IP="20.82.192.28"
 
 #Create and configure firewall
 az network public-ip create -g $RG -n $FWPUBLICIP_NAME -l $LOC --sku "Standard"
@@ -24,3 +26,17 @@ $FWPRIVATE_IP=$(az network firewall show -g $RG -n $FWNAME --query "ipConfigurat
 az network route-table create -g $RG -l $LOC --name $FWROUTE_TABLE_NAME
 az network route-table route create -g $RG --name $FWROUTE_NAME --route-table-name $FWROUTE_TABLE_NAME --address-prefix 0.0.0.0/0 --next-hop-type VirtualAppliance --next-hop-ip-address $FWPRIVATE_IP --subscription $SUBID
 az network route-table route create -g $RG --name $FWROUTE_NAME_INTERNET --route-table-name $FWROUTE_TABLE_NAME --address-prefix $FWPUBLIC_IP/32 --next-hop-type Internet
+
+#Firewall Rules
+# Add FW Network Rules
+az network firewall network-rule create -g $RG -f $FWNAME --collection-name 'aksfwnr' -n 'apiudp' --protocols 'UDP' --source-addresses '*' --destination-addresses "AzureCloud.$LOC" --destination-ports 1194 --action allow --priority 100
+az network firewall network-rule create -g $RG -f $FWNAME --collection-name 'aksfwnr' -n 'apitcp' --protocols 'TCP' --source-addresses '*' --destination-addresses "AzureCloud.$LOC" --destination-ports 9000
+az network firewall network-rule create -g $RG -f $FWNAME --collection-name 'aksfwnr' -n 'time' --protocols 'UDP' --source-addresses '*' --destination-fqdns 'ntp.ubuntu.com' --destination-ports 123
+
+# Add FW Application Rules
+az network firewall application-rule create -g $RG -f $FWNAME --collection-name 'aksfwar' -n 'fqdn' --source-addresses '*' --protocols 'http=80' 'https=443' --fqdn-tags "AzureKubernetesService" --action allow --priority 100
+
+# Associate route table with next hop to Firewall to the AKS subnet
+az network vnet subnet update -g $RG --vnet-name $VNET_NAME --name $AKSSUBNET_NAME --route-table $FWROUTE_TABLE_NAME
+
+az network firewall nat-rule create --collection-name exampleset --destination-addresses $FWPUBLIC_IP --destination-ports 80 --firewall-name $FWNAME --name inboundrule --protocols Any --resource-group $RG --source-addresses '*' --translated-port 80 --action Dnat --priority 100 --translated-address $SERVICE_IP
